0e7a94c04f1308f05416c0932979a7f97f3fc8d8
Berkay <berkay.ergun16@gmail.com>
2022-01-30 21:20:58 +0300

    base

--- lib/core/base/state/base_state.dart
+++ lib/core/base/state/base_state.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class BaseState<T extends StatefulWidget> extends State<T> {
-  ThemeData get themeData => Theme.of(context);
+
+  ThemeData get  themeData  => Theme.of(context);
 
   //!ihtiyacımız olan yerlerde mediaquery alıyoruz.
   double dynamicHeight(double value) => MediaQuery.of(context).size.height * value;
